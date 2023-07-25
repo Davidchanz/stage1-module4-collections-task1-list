c1a40e40fd50079a37db533c3dc87328ce6c4892
Davidchanz <tovliskatsi@gmail.com>
2023-07-25 15:28:21 +0400

    Collections Task 1 List completed

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,21 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int v1 = Integer.parseInt(a);
+        int v2 = Integer.parseInt(b);
+        return function(v1) > function(v2) ? 1 :
+                function(v1) < function(v2) ? -1 :
+                        v1 > v2 ? 0 : -1;
+    }
+
+    private int function(int v){
+        return 5*v*v+3;
     }
 }
